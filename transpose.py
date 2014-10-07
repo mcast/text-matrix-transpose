2df2e0eb5d867987340c87687adc4fca31251d87
Matthew Astley <codeonly$mca@t8o.org>
2014-10-07 17:59:52 +0100

    checkpoint

--- transpose.py
+++ transpose.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 import sys
 import os
 
@@ -8,37 +9,49 @@ def main():
     path_out = os.path.basename(path_in)+'.transposed'
     separator = ' '
 
-    d_seek = {}
+    with open(path_in) as fd_in:
+        line = fd_in.readline()
+    rows2 = cols1 = len(line.split(separator))
+    del line
+
+    in_seek = {}
+    out_len = {}
+    for x in range(cols1):
+        out_len[x] = 0
+
     with open(path_in) as fd_in:
         i = 0
         print('indexing')
         while True:
             tell = fd_in.tell()
-            if fd_in.readline() == '':
+            line = fd_in.readline()
+            if line == '':
                 break
-            d_seek[i] = tell
+            row = line.split(separator)
+            if len(row) != cols1:
+                raise "len mismatch ln" + i
+            for x in range(cols1):
+                out_len[x] += len(row[x])
+            in_seek[i] = tell
             i += 1
         print('indexed')
     cols2 = rows1 = i
 
-    with open(path_in) as fd_in:
-        line = fd_in.readline()
-    rows2 = cols1 = len(line.split(separator))
-    del line
+#blech    print('in_seek = ' + in_seek.values().tostring())
 
     with open(path_in) as fd_in, open(path_out, 'w') as fd_out:
         print('transposing')
         for row2 in range(rows2):
             print('row', row2)
             for row1 in range(rows1):
-                fd_in.seek(d_seek[row1])
+                fd_in.seek(in_seek[row1])
                 s = ''
                 while True:
                     char = fd_in.read(1)
                     if char == separator or char == '\n':
                         break
                     s += char
-                d_seek[row1] += len(s)+1
+                in_seek[row1] += len(s)+1
                 if row1+1 < rows1:
                     fd_out.write('{} '.format(s))
                 else:
