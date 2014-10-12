211905dcf9528f3f336ed0eacbc552c4bafd2711
Matthew Astley <codeonly$mca@t8o.org>
2014-10-12 19:35:46 +0100

    implement mem_budget for buffered rowsT

--- transpose.py
+++ transpose.py
@@ -16,12 +16,14 @@ class TextTransposer:
         self.rowU_tell = {} # seek here to get next value from within the row
         self.rowT = {}      # collect output
         self.collected_size = 0 # in bytes, ignoring python overhead
+        self.mem_budget = 100 * 1024 * 1024 # in bytes, ignoring Python overheads & rowU_tell
 
         # no data yet
         self.colsU = -1
         self.rowsU = -1
         self.longcell = 0       # without separator
         self.longrowU = 0       # with \n
+        self.colU_keepn = -1
 
     def rowsT(self):
         return self.colsU
@@ -45,7 +47,7 @@ class TextTransposer:
             self.rowU_tell[rowU] = self.fd_in.tell()
         else:
             self.fd_in.seek(self.rowU_tell[0])
-            keep_colU = range(colU_start, self.colsU)
+            keep_colU = range(colU_start, min(self.colsU, colU_start + self.colU_keepn))
             print("    set keep_colU = %s" % keep_colU)
 
         while True:
@@ -60,18 +62,20 @@ class TextTransposer:
                 if self.colsU < 0:
                     # first row seen ever
                     self.colsU = len(colsU)
-                    keep_colU = range(colU_start, int(self.colsU / 2))
+                    self.colU_keepn = self.colsU # initially keep all
+                    keep_colU = range(colU_start, self.colsU)
                     print("    set keep_colU = %s" % keep_colU)
                 elif len(colsU) != self.colsU:
                     raise Exception("%d: column count mismatch (got %d, expect %d)" %
                                     (rowU+1, len(colsU), self.colsU))
-                rowU += 1
-                self.rowU_tell[rowU] = self.fd_in.tell()
                 longest = max(map(len, colsU))
                 if longest > self.longcell:
                     self.longcell = longest
+                    keep_colU = self.set_memlimit(keep_colU)
                 if len(line) > self.longrowU:
                     self.longrowU = len(line)
+                rowU += 1
+                self.rowU_tell[rowU] = self.fd_in.tell()
             else:
                 rowU += 1
 
@@ -90,6 +94,28 @@ class TextTransposer:
         print("  done loop %d, next is col %d" % (passnum, keep_colU.stop))
         return (keep_colU.stop, None)[keep_colU.stop == self.colsU]
 
+    def set_memlimit(self, keep_colU):
+        """Longest cell (longcell) was increased.  We may need to store fewer
+        colsU == rowsT in stay in memory budget."""
+        bytes_per_rowT = (self.longcell + 1) * self.rowsT() # +1 for sep
+        new_colU_keepn = int(self.mem_budget / bytes_per_rowT)
+        if new_colU_keepn >= self.colU_keepn:
+            # plenty, continue
+            return keep_colU
+        elif new_colU_keepn < 1:
+            # this implemention doesn't support streaming one row at a
+            # time, it will always buffer it
+            mib = 1024*1024.0
+            raise Exception("Memory budget (%.3f MiB) insufficient for one row (up to %.3f MiB from longcell:%d bytes)" %
+                            (self.mem_budget / mib, bytes_per_rowT / mib, self.longcell))
+        else:
+            # time to downsize
+            purge = range(new_colU_keepn, keep_colU.stop)
+            if self.rowT != {}: # nothing to purge yet, if we're called on first row
+                for x in purge:
+                    del self.rowT[x]
+            self.colU_keepn = new_colU_keepn
+            return range(keep_colU.start, new_colU_keepn)
 
 def main():
 
