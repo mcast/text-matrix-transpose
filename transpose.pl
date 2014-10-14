c2698f91a250d4be8afdc3f17853cd011e6cd2d3
Matthew Astley <codeonly$mca@t8o.org>
2014-10-14 21:08:15 +0100

    direct Perl translation (working)

--- transpose.pl
+++ transpose.pl
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 
 use IO::Handle;
-use List::Util qw( min );
+use List::Util qw( min max );
 
 
 =head1 NAME
@@ -35,7 +35,7 @@ sub main {
 }
 
 sub new {
-  my ($pkg, $fh_in, $fh_out) = @_;
+  my ($pkg, $fd_in, $fd_out) = @_;
 
   my $self =
     { fd_in => $fd_in,
@@ -48,25 +48,34 @@ sub new {
       # no data yet
       colsU => -1,
       rowsU => -1,
-      longcell => 0,		# without separator
-      longrowU => 0,		# with \n
-      shortrowU => -1,		# with \n
+      longcell => 0,            # without separator
+      longrowU => 0,            # with \n
+      shortrowU => -1,          # with \n
       colU_keepn => -1,
     };
   bless $self, $pkg;
   return $self;
 }
 
-sub rowsT { $_->[0]->{colsU} }
-sub colsT { $_->[0]->{rowsU} }
+sub rowsT { $_[0]->{colsU} }
+sub colsT { $_[0]->{rowsU} }
 
-sub colsU { $_->[0]->{colsU} }
-sub rowsU { $_->[0]->{rowsU} }
+sub _init {
+  my ($pkg) = @_;
+  foreach my $prop (qw( colsU rowsU colU fd_in fd_out longcell longrowU shortrowU separator ),
+                    qw( bytes_per_rowT ), # init in set_memlimit
+                    qw( rowT colU_keepn rowU_tell fd_in_size mem_budget )) {
+    my $code = sub {
+      my ($self, @set) = @_;
+      ($self->{$prop}) = @set if @set;
+      return $self->{$prop};
+    };
+    no strict 'refs';
+    *{"${pkg}::$prop"} = $code;
+  }
+  return;
+}
 
-sub colU_keepn { $_->[0]->{colU_keepn} }
-
-sub fd_in { $_->[0]->{fd_in} }
-sub fd_out { $_->[0]->{fd_out} }
 
 sub loop_until_done {
   my ($self) = @_;
@@ -74,7 +83,7 @@ sub loop_until_done {
   my $nextcolU = 0;
   while (1) {
     $nextcolU = $self->loop($passnum, $nextcolU);
-    printf "  next loop = %s\n", nextcolU;
+    printf "  next loop = %s\n", defined $nextcolU ? $nextcolU : 'None';
     last unless defined $nextcolU;
     $passnum ++;
   }
@@ -85,21 +94,190 @@ sub loop {
   my ($self, $passnum, $colU_start) = @_;
   my $rowU = 0;
   my $keep_colU; # init on first row, or subsequent pass
+  my ($need_widthU, $need_bytes);
+
   if ($passnum == 0) {
-    $self->{rowU_tell}[$rowU] = $self->fd_in->tell();
-    $self->{fd_in_size} = -s $self->fd_in;
+    $self->rowU_tell->[$rowU] = $self->fd_in->tell();
+    $self->fd_in_size(-s $self->fd_in);
   } else {
-    $self->fd_in->seek( $self->{rowU_tell}[0] )
-___ or die ?;
+    $self->fd_in->seek($self->rowU_tell->[0], 0)
+      or die "seek failed: $!";
 
     $keep_colU = Range->new($colU_start, min($self->colsU, $colU_start + $self->colU_keepn));
-
+    $need_widthU = $keep_colU->size;
+    $need_bytes = ($self->longcell + 1) * $need_widthU;
+    printf "    set keep_colU = %s\n", $keep_colU->to_string;
   }
 
-  
+  if ($passnum == 0) {
+    while (1) {
+      printf "  rowU:%d\n", $rowU unless $rowU % 10000;
+      my $line = $self->fd_in->getline;
+      last unless defined $line; # eof
+
+      my @colsU;
+      if ($self->colsU < 0) {
+        # first row seen ever
+        @colsU = split $self->separator, $line;
+        shift @colsU while $colsU[0] eq ''; # leading space on the row
+        chomp $colsU[-1];
+
+        $self->colsU(scalar @colsU);
+        $self->colU_keepn(scalar @colsU); # initially keep all
+        $keep_colU = Range->new($colU_start, $self->colsU);
+        printf "    set keep_colU = %s\n", $keep_colU->to_string;
+      } else {
+        $need_widthU = $keep_colU->size;
+        # XXX: here we used to check actual #colsU matched expected (check matrix not ragged)
+        # now we only discover where lines are short
+        @colsU = split $self->separator, $line, $need_widthU + 1;
+        # XXX: using Perl built-in means we aren't checking $need_widthU columns are available
+        chomp $colsU[-1]; # has effect only on last column
+      }
+
+      if ($self->shortrowU == -1 || length($line) < $self->shortrowU) {
+	$self->shortrowU(length($line));
+	printf "    shortrow:%d\n", $self->shortrowU;
+      }
+      if (length($line) > $self->longrowU) {
+	$self->longrowU(length($line));
+          printf "    longrow:%d\n", $self->longrowU;
+      }
+
+      my $longest = max(map { length($_) } @colsU);
+      $rowU ++;
+      if ($longest > $self->longcell) {
+	$self->longcell($longest);
+	$keep_colU = $self->set_memlimit($keep_colU, $self->est_rowsU($rowU));
+      } elsif ($rowU % 1000 == 0) {
+	$keep_colU = $self->set_memlimit($keep_colU, $self->est_rowsU($rowU));
+      }
+      $self->rowU_tell->[$rowU] = $self->fd_in->tell();
+      $self->stash_rowU($rowU, $keep_colU, \@colsU);
+    }
+
+  } else {
+    while (1) {
+      printf "  rowU:%d\n", $rowU unless $rowU % 10000;
+      last if $rowU == $self->rowsU; # eof
+      $self->fd_in->seek($self->rowU_tell->[$rowU], 0)
+	or die "seek failed: $!";
+      my $txt;
+      my $nread = $self->fd_in->read($txt, $need_bytes);
+      die "$rowU: $! on input" unless defined $nread;
+      my @colsU = split $self->separator, $txt, $need_widthU + 1;
+      $rowU ++;
+      $self->stash_rowU($rowU, $keep_colU, \@colsU);
+    }
+  }
+
+  $self->rowsU($rowU) if $passnum == 0;
+  $self->dump_kept($keep_colU);
+  if ($passnum == 0) {
+    # re-evaluate, now we have a real rowsU
+    $self->set_memlimit($keep_colU, $rowU);
+    if ($self->colU_keepn == 1) {
+      my $mib = 1024*1024;
+      warn sprintf("Streaming only! Memory budget (%.3f MiB) insufficient for one row (up to %.3f MiB from colsT:%d * longcell:%d bytes)",
+                   $self->mem_budget / $mib,
+                   $self->bytes_per_rowT / $mib,
+                   $self->colsT, $self->longcell);
+    }
+  }
+
+  printf "  done loop %d, next is col %d\n", $passnum, $keep_colU->stop;
+
+  return $keep_colU->stop == $self->colsU ? undef : $keep_colU->stop;
 }
 
-__PACKAGE__->main(@ARGV) if caller();
+# sub splitn : not required
+
+# During first pass, estimate rowsU count from available data.
+# Early over-estimates may reduce the useful work done by passnum == 0.
+# Might do better counting actual memory usage, or bytes stashed in rowT[].
+sub est_rowsU {
+  my ($self, $curr_rowU) = @_;
+  my $fpos = $self->fd_in->tell();
+  my $min_rows = $self->fd_in_size / $self->longrowU;
+  my $seen_frac = $fpos / $self->fd_in_size;
+  if ($seen_frac < 0.25) {
+    # near start of file => inaccurate stats, give a low estimate
+    return max(int($min_rows), $curr_rowU);
+  } else {
+    my $avglen_rows = $curr_rowU * $self->fd_in_size / $fpos;
+    # max_rows = self.fd_in_size / self.shortrowU
+    return max(int($avglen_rows), $curr_rowU);
+  }
+}
+
+sub stash_rowU {
+  my ($self, $rowU, $keep_colU, $colsU_l) = @_;
+  my $stashable = Range->new($keep_colU->start + 1, $keep_colU->stop);
+  # We don't stash colsU[0], because we can stream
+  # it during reading.  Still, keep it in the range to simplify
+  # other logic / not yet refactored
+  print { $self->fd_out } $self->separator unless $rowU == 1; # non-stashed, sep before
+  foreach my $x ($stashable->start .. $stashable->stop - 1) {
+    push @{ $self->rowT->{$x} }, $colsU_l->[ $x - $keep_colU->start ];
+  }
+  my $nonstash = $colsU_l->[0]; # non-stashed
+  print { $self->fd_out } $nonstash;
+  $self->rowU_tell->[ $rowU-1 ] += length($nonstash) + 1; # +1 for sep after
+  return;
+}
+
+sub dump_kept {
+  my ($self, $keep_colU) = @_;
+  print { $self->fd_out } "\n"; # close the non-stashed output
+  my $widthT = Range->new(0, $self->colsT);
+  my $lastT = $self->colsT - 1;
+  my $stashable = Range->new($keep_colU->start + 1, $keep_colU->stop);
+  foreach my $y ($stashable->start .. $stashable->stop - 1) { # XXX: weirdly non-perl
+    foreach my $x ($widthT->start .. $widthT->stop - 1) {
+      my $cell = $self->rowT->{$y}[$x];
+      $self->rowU_tell->[$x] += length($cell) + 1; # +1 for sep
+      print { $self->fd_out } $cell, $x == $lastT ? "\n" : $self->separator;
+    }
+  }
+  $self->rowT({});
+  return;
+}
+
+# Longest cell (longcell) was increased.  We may need to store fewer
+# colsU == rowsT in stay in memory budget.
+sub set_memlimit {
+  my ($self, $keep_colU, $rowsU) = @_;
+  my $bytes_per_rowT = ($self->longcell + 1) * $rowsU; # +1 for sep
+  $self->bytes_per_rowT($bytes_per_rowT);
+  my $old_colU_keepn = $self->colU_keepn;
+  my $new_colU_keepn = int($self->mem_budget / $bytes_per_rowT
+                           / 1.00); # XXX: empirical fudge factor ..?
+  $self->colU_keepn( min($old_colU_keepn, $new_colU_keepn + 1) ); # =1 for the non-stashed
+  if ($new_colU_keepn != $old_colU_keepn) {
+    printf "      set_memlimit(%s, %.3f MiB, rowsU:%d) bytes_per_rowT:%d for longcell:%d gives colU:%d\n",
+      $keep_colU->to_string, $self->mem_budget / (1024*1024), $rowsU,
+        $bytes_per_rowT, $self->longcell, $self->colU_keepn;
+  }
+  if ($self->colU_keepn >= $old_colU_keepn) {
+    # plenty, continue
+    # new_colU_keepn could be larger than current, but that won't bring rowT[] elements back
+    return $keep_colU;
+  } else {
+    # time to downsize
+    my $purge = Range->new($self->colU_keepn, $keep_colU->stop);
+    if (keys %{ $self->rowT }) {
+      printf "      purge rowT[%s]\n", $purge->to_string;
+      foreach my $i ($purge->start .. $purge->stop - 1) {
+        delete @{ $self->rowT }{ $i };
+      }
+    } # else nothing to purge yet, we're called on first row
+    return Range->new($keep_colU->start, $self->colU_keepn);
+  }
+}
+
+
+__PACKAGE__->_init;
+__PACKAGE__->main(@ARGV) unless caller();
 
 1;
 
@@ -110,7 +288,19 @@ sub new {
   my ($pkg, $start, $stop) = @_;
   return bless [ $start, $stop ], $pkg;
 }
-sub start { $_->[0][0] }
-sub stop  { $_->[0][1] }
+
+sub start { $_[0][0] }
+sub stop  { $_[0][1] }
+
+sub size  {
+  my ($self) = @_;
+  return $self->stop - $self->start;
+}
+
+sub to_string {
+  my ($self) = @_;
+  return sprintf('[%d, %d)', # Interval notation
+		 $self->start, $self->stop);
+}
 
 1;
